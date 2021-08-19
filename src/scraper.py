c0325bb30c1a58f2ffee508ce38a42c858856630
th3outcast <amatajosh@gmail.com>
2021-08-19 20:11:47 +0100

    Add paper download link extractor

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,12 @@
 import requests
+import urllib.parse
+from bs4 import BeautifulSoup
+
 
 API_URL = "https://api.semanticscholar.org/graph/v1"
-
+timeout = 5
+limit = 5
+offset = 0
 
 """
     Query semanticscholar.org api endpoint for paper
@@ -9,36 +14,61 @@ API_URL = "https://api.semanticscholar.org/graph/v1"
 def semantic_scholar_query(paper):
     params = {
         "query": paper,
-        "offset": 0,
-        "limit": 5,
+        "offset": offset,
+        "limit": limit,
     }
 
     # request paper from api endpoint
     r = requests.get(API_URL + "/paper/search",
             params=params,
-            timeout=3)
+            timeout=timeout)
 
     # parse json response
     if r.status_code == 200:
-        resp = r.json()
-        if len(resp) == 1 and 'error' in data:
-            resp = {}
+        data = r.json()
+        if len(data) == 1 and 'error' in data:
+            data = {}
     elif r.status_code == 429:
         raise ConnectionRefusedError("HTTP status 429: Too Many Requests.")
-    return resp
+    return data
 
 
-def parse_links(res):
+def parse_titles(res):
     base_url = "https://semanticscholar.org/paper/"
 
     # dictionary to hold titles and their links
     titles = {}
 
     for i in range(len(res['data'])):
-        uri = base_url + res['data'][i]['title'].replace(" ", "-")
+        uri = res['data'][i]['title'].replace(" ", "-")
         uri += '/' + res['data'][i]['paperId']
-        titles[res['data'][i]['title']] = uri
+
+        # parse special url characters
+        titles[res['data'][i]['title']] = base_url + urllib.parse.quote(uri, safe='-/')
 
     return titles
+
+
+def extract_download(url):
+    links = []
+    r = requests.get(url,
+            timeout=timeout)
+    result_html = BeautifulSoup(r.text, "html.parser")
+
+    for i in result_html.findAll("a", {"data-selenium-selector" : "paper-link"}):
+        links.append(
+                i.get('href')
+                )
+
+    return links
+
+
+def extract_paper_links(parsed_titles):
+    paper_link_map = {}
+    for k, v in parsed_titles.items():
+        paper_link_map[k] = extract_download(v)
+
+    return paper_link_map
+
 
 if __name__ == "__main__":
